935ef44053ba763f63b74adc9ede5e77048c1c9c
minkyu <minkyu1128@gmail.com>
2022-01-12 12:14:35 +0900

    PID 추가

--- src/main/java/com/joalifemall/demo/JoalifemallDemoApplication.java
+++ src/main/java/com/joalifemall/demo/JoalifemallDemoApplication.java
@@ -2,12 +2,16 @@ package com.joalifemall.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class JoalifemallDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JoalifemallDemoApplication.class, args);
+		//SpringApplication.run(JoalifemallDemoApplication.class, args);
+		SpringApplication application = new SpringApplication(JoalifemallDemoApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());	//PID(Process ID 작성)
+		application.run(args);
 	}
 
 }
